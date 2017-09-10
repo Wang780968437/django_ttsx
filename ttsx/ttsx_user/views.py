95895b39f424055c0891aa4a002fbd0eb9255331
zhuhongbo <632996921@qq.com>
2017-09-10 12:16:22 +0800

    用户模型类创建

---
+++ ttsx/ttsx_user/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def register(request):
+    return render(request,'ttsx_user/register.html')
+
+def zhanshi(request):
+    return HttpResponse('zhanshi')
