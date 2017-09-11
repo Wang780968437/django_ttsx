d2e425a2ecc0d065def1ddcabab0c3b891609e70
zbh159265 <1784261210@qq.com>
2017-09-11 09:23:28 +0800

    上传模型

--- ttsx/tt_goods/urls.py
+++ ttsx/tt_goods/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
-from . import views
+
 
 urlpatterns=[
-    url('^$',views.index),
-    url('^list(\d+)_(\d+)_(\d+)/$',views.list),
-    url('^(\d+)/$',views.detail)
+
 ]
