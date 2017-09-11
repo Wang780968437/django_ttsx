c2c448a6eaba7f6358e34e5dba391a0449987dba
w_y_x <Wang_yu_xiang@outlook.com>
2017-09-11 10:26:27 +0800

    修改忽略文件

--- ttsx/tt_cart/urls.py
+++ ttsx/tt_cart/urls.py
@@ -2,9 +2,4 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [
-    url(r'^show_cart/$', views.show_cart),
-    url(r'^add_cart/$', views.add_cart),
-    url(r'^detail/$', views.detail),
-]
 
--- ttsx/tt_cart/views.py
+++ ttsx/tt_cart/views.py
@@ -3,26 +3,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def show_cart(request):
-    return render(request, 'tt_cart/cart.html')
 
-
-def detail(request):
-    return render(request, 'tt_goods/detail.html')
-
-def add_cart(request):
-    good_name = request.GET.get('$good_name')
-    show_pirze = request.GET.get('$show_pirze')
-    num_show = request.GET.get('$num_show')
-    goods_detail_pic = request.GET.get('$goods_detail_pic')
-    context = {'good_name': good_name, 'show_pirze': show_pirze, 'num_show': num_show, 'goods_detail_pic': goods_detail_pic}
-
-    return render(request, 'tt_cart/cart.html', context)
-
-from django.shortcuts import render
-
-# Create your views here.
-
-def cart(request):
-    return render(request, 'tt_cart/')
 
