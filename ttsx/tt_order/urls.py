7de387073ec85435ea81c4910e769a2f48e75cd5
mxj <mxj_email@163.com>
2017-09-11 18:25:17 +0800

    修改views

--- ttsx/tt_order/urls.py
+++ ttsx/tt_order/urls.py
@@ -4,5 +4,7 @@ from django.conf.urls import url
 from tt_order import views
 
 urlpatterns = [
-    url(r'to_pay/$',views.show_order),
+    url(r'to_pay/$',views.verify_order),
+    url(r'order_list/$',views.order_list),
+    url(r'all_order/$',views.all_order)
 ]
--- ttsx/tt_order/views.py
+++ ttsx/tt_order/views.py
@@ -1,10 +1,30 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 
-
-def show_order(request):
+# 从购物车提交订单后转到确认订单页面
+def verify_order(request):
 
 
     return render(request,"tt_order/place_order.html")
+
+# 订单列表
+def order_list(request):
+
+    return JsonResponse({})
+
+
+def all_order(request):
+
+    # 判断用户是否登陆
+
+    # 拿到登陆的用户名
+
+    # 根据当前登陆的用户名，获取用户订单信息
+    orderinfo = OrderInfo.objects.filter(user__uname='')
+
+    context = {}
+    return render(request,"tt_order/user_center_order.html",context)
