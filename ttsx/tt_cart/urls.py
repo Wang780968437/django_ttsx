6d6394e09dbdfaea3db02314cd5f1240debe2f36
w_y_x <Wang_yu_xiang@outlook.com>
2017-09-11 09:51:51 +0800

    保存

---
+++ ttsx/tt_cart/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^show_cart/$', views.show_cart),
+    url(r'^add_cart/$', views.add_cart),
+    url(r'^detail/$', views.detail),
+]
