c794aa8713cf0893a2e6ea97b0fb2ec7a0a3ee48
mxj <mxj_email@163.com>
2017-09-13 20:47:22 +0800

    修改views

--- ttsx/tt_order/views.py
+++ ttsx/tt_order/views.py
@@ -9,11 +9,17 @@ from ttsx_user.models import UserInfo
 
 # 从购物车提交订单后转到确认订单页面
 def verify_order(request):
-    # 获取登录用户id
-    uid = request.session.get('user_id')
-    print(uid)
-    # 获取该用户购物车
-    carts = CartInfo.objects.filter(user_id = uid)
+    # 获取登录用户名
+    name = request.session.get('user_name')
+    print(name)
+    # 获取该用户提交的购物车信息,列表元素是字符串
+    cart_ids = request.GET.getlist('cart_id')
+    # 遍历转换为数字
+    lists = []
+    for i in cart_ids:
+        lists.append(int(i))
+
+    carts = CartInfo.objects.filter(pk__in=lists)
     print(carts)
     context = {'cartlist':carts}
 
@@ -33,6 +39,7 @@ def all_order(request):
     print(name)
     # 根据当前登陆的用户名，获取用户所有订单信息
     orderinfolist = OrderInfo.objects.filter(user__uname = name)
+    print(orderinfolist)
     # 上下文
     context = {"list":orderinfolist}
     return render(request,"tt_order/user_center_order.html",context)
