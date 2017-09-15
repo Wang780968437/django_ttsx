91cd58249019f047e0a1ef86765e7c08095d2a5a
mxj <mxj_email@163.com>
2017-09-15 10:31:45 +0800

    cart的url冲突

--- ttsx/tt_order/urls.py
+++ ttsx/tt_order/urls.py
@@ -1,10 +1,10 @@
 # -*- conding: utf-8 -*-
 from django.conf.urls import url
 
-from tt_order import views
+from . import views
 
 urlpatterns = [
-    url(r'to_pay/$',views.verify_order),
-    url(r'order_list/$',views.order_list),
-    url(r'all_order/$',views.all_order)
+    url(r'^to_pay/$',views.verify_order),
+    url(r'^order_list/$',views.order_list),
+    url(r'^all_order/$',views.all_order),
 ]
