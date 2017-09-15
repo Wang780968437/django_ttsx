05c288788a1286c39b62a6a80de888e048023b98
mxj <mxj_email@163.com>
2017-09-15 09:53:52 +0800

    保存

---
+++ ttsx/tt_goods/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import tinymce.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='GoodsInfo',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('gtitle', models.CharField(max_length=20)),
+                ('gpic', models.ImageField(upload_to='goods')),
+                ('gprice', models.DecimalField(decimal_places=2, max_digits=5)),
+                ('isDelete', models.BooleanField(default=False)),
+                ('gunit', models.CharField(default='500g', max_length=20)),
+                ('gclick', models.IntegerField()),
+                ('gjianjie', models.CharField(max_length=200)),
+                ('gkucun', models.IntegerField()),
+                ('gcontent', tinymce.models.HTMLField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='TypeInfo',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('ttitle', models.CharField(max_length=20)),
+                ('isDelete', models.BooleanField(default=False)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='goodsinfo',
+            name='gtype',
+            field=models.ForeignKey(to='tt_goods.TypeInfo'),
+        ),
+    ]
